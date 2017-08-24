bff615ebb21c6fc82dcc8d1bee87c4b29c270043
Taylor Peat <taylorpeat@hotmail.com>
2017-08-23 22:58:01 -0400

    video and category tests

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Video do
   it 'saves itself' do
-    video = Video.new(title: 'monk', description: 'very nice', rating: 3.1, category: 'drama')
+    video = Video.create(title: 'monk', description: 'very nice', rating: 3.1, category_id: 2)
     video.save
     expect(Video.first).to eq(video)
   end
