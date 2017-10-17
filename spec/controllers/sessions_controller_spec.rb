6ed51de92009d9bb0bbaa812740a908fcad610e0
Taylor Peat <taylorpeat@hotmail.com>
2017-10-17 15:21:25 -0400

    session destroy controller test

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,29 +1,35 @@
 require 'spec_helper'
 
 describe SessionsController do
-  describe "GET new" do
+  describe "GET new" do  
     it "redirects to home if logged in" do
       session[:user_id] = Fabricate(:user).id
       get :new
       expect(response).to redirect_to home_path
     end
+    
     it "doesn't redirect to home if not logged in" do
       get :new
       expect(response).not_to redirect_to home_path
     end
   end
+  
   describe "POST create" do
     context "valid credentials" do
+      let(:user) { Fabricate(:user) }
+
       before do
-        @user = Fabricate(:user)
-        post :create, email: @user.email, password: @user.password
+        post :create, email: user.email, password: user.password
       end
+      
       it "creates session for user" do
-        expect(session[:user_id]).to eq(@user.id)
+        expect(session[:user_id]).to eq(user.id)
       end
+      
       it "redirects to home" do
         expect(response).to redirect_to home_path
       end
+      
       it "sets the notice" do
         expect(flash[:notice]).not_to be_blank
       end
@@ -33,22 +39,37 @@ describe SessionsController do
       before do
         post :create, email: nil, password: "password"
       end
+      
       it "does not create session" do
         expect(session[:user_id]).to be_nil
       end
+      
       it "sets the error message" do
         expect(flash[:error]).not_to be_blank
       end
+      
       it "redirects to the sign in page" do
         expect(response).to redirect_to sign_in_path
       end
     end
   end
+  
   describe "GET destroy" do
-    it "clears the session for the user" do
+    before do
       session[:user_id] = Fabricate(:user).id
       get :destroy
+    end
+    
+    it "clears the session for the user" do
       expect(session[:user_id]).to be_nil
+    end
+
+    it "redirects to root" do
+      expect(response).to redirect_to root_path
+    end
+    
+    it "sets flash notice" do
+      expect(flash[:notice]).to eq("You have signed out.")
     end
   end
 end
