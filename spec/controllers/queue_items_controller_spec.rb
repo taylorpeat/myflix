2bfbc38566a68a33763a7b199e885786553009ee
Taylor Peat <taylorpeat@hotmail.com>
2017-10-31 15:34:35 -0400

    Create queue items fabricator and controller

--- spec/controllers/queue_items_controller_spec.rb
+++ spec/controllers/queue_items_controller_spec.rb
@@ -1,5 +1,27 @@
 require 'spec_helper'
 
 describe QueueItemsController do
+  describe "GET index" do
+    let(:video1) { Fabricate(:video) }
+    let(:video2) { Fabricate(:video) }
+    let(:user) { Fabricate(:user) }
 
+    let(:queue_items) do
+      queue_item1 = Fabricate(:queue_item, video_id: video1.id, user_id: user.id )
+      queue_item2 = Fabricate(:queue_item, video_id: video2.id), user_id: user.id )
+    end
+
+    context "With authenticated user" do
+      before do
+        session[:user_id] = user.id
+      end
+
+      it "sets @queue_items" do
+        expect(:queue_items).to match_array(queue_items)
+      end
+    end
+
+    context "With unauthenticated user" do
+    end
+  end
 end
--- spec/controllers/reviews_controller_spec.rb
+++ spec/controllers/reviews_controller_spec.rb
@@ -3,29 +3,36 @@ require 'spec_helper'
 describe ReviewsController do
   describe 'POST create' do
     let(:video) { Fabricate(:video) }
-    context 'authenticated user' do  
+    
+    context 'authenticated user' do    
       before do
         session[:user_id] = Fabricate(:user).id
       end
       
       context 'valid input' do
         let(:review) { Fabricate.attributes_for(:review, video: video) }
+        
         before do |b|
           post :create, review: review, video_id: video.id unless b.metadata[:skip_before]
         end
+        
         it 'creates new review' do
           expect(Review.count).to eq(1)
         end
+
         it 'creates a review associated with the video' do
           expect(Review.first.video_id).to eq(video.id)
         end
+
         it 'refreshes page' do
           expect(response).to redirect_to(video_path(video.id))
         end
+
         it 'doesnt display an error message' do
           expect(flash[:error]).to be_blank
         end
       end
+
       context 'invalid input' do
         let(:invalid_review) { { rating: 1, content: '', video_id: video.id } }
         
@@ -36,16 +43,20 @@ describe ReviewsController do
         it "doesn't create review" do
           expect(Review.count).to eq(0)
         end
+
         it 'displays error message' do
           expect(flash[:error]).not_to be_blank
         end
+
         it 'refreshes page' do
           expect(response).to redirect_to(video_path(video.id))
         end
       end
     end
+
     context 'unauthenticated user' do
       let(:review) { Fabricate.attributes_for(:review, video: video) }
+      
       it "redirects to sign in page if unauthenticated" do
         post :create, review: review, video_id: video.id
         expect(response).to redirect_to sign_in_path
