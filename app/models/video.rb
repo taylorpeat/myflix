b50c74bcbd5656e9c7cd65d0eed6cea1b7d55c22
Taylor Peat <taylorpeat@hotmail.com>
2017-11-01 17:24:55 -0400

    Add categories controller

--- app/models/video.rb
+++ app/models/video.rb
@@ -10,6 +10,11 @@ class Video < ActiveRecord::Base
     where("title LIKE ?", "%#{search_term}%").order("created_at ASC")
   end
 
+  def self.search_by_category(category_id)
+    return [] if category_id.blank?
+    where({ category_id: category_id })
+  end
+
   def rating
     return nil if reviews.empty?
     (reviews.reduce(0) { |sum, r| r.rating.to_f + sum } / reviews.count).round(1)
--- spec/models/queue_item_spec.rb
+++ spec/models/queue_item_spec.rb
@@ -1,4 +1,12 @@
 require 'spec_helper'
 
 describe QueueItem do
+  it { should belong_to(:user) }
+  it { should belong_to(:video) }
+
+  describe "#video_title" do
+    it "returns the title of the associated video" do
+    
+    end
+  end
 end
