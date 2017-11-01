1e1ca55b7a1d2111bf398e7f2d044c55259991c1
Taylor Peat <taylorpeat@hotmail.com>
2017-11-01 17:53:06 -0400

    Finish Queue Item Model Test

--- spec/models/queue_item_spec.rb
+++ spec/models/queue_item_spec.rb
@@ -6,7 +6,26 @@ describe QueueItem do
 
   describe "#video_title" do
     it "returns the title of the associated video" do
-    
+      video = Fabricate(:video)
+      queue_item = Fabricate(:queue_item, video: video)
+      expect(queue_item.video_title).to eq(video.title.titleize)
+    end
+  end
+
+  describe "#rating" do
+    it "returns the rating from the review when the review is present" do
+      user = Fabricate(:user)
+      video = Fabricate(:video)
+      review = Fabricate(:review, video: video, user: user, rating: 4)
+      queue_item = Fabricate(:queue_item, video: video, user: user)
+      expect(queue_item.rating).to eq(4)
+    end
+
+    it "returns nil when the review is not present" do
+      user = Fabricate(:user)
+      video = Fabricate(:video)
+      queue_item = Fabricate(:queue_item, video: video, user: user)
+      expect(queue_item.rating).to be_nil
     end
   end
 end
