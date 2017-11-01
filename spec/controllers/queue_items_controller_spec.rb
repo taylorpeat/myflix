831cd05734460ddf41dbe6c04043d6c3c6aec93a
Taylor Peat <taylorpeat@hotmail.com>
2017-11-01 00:27:10 -0400

    assign queue items in controller and test

--- spec/controllers/queue_items_controller_spec.rb
+++ spec/controllers/queue_items_controller_spec.rb
@@ -9,19 +9,25 @@ describe QueueItemsController do
     let(:queue_items) do
       queue_item1 = Fabricate(:queue_item, video: video1, user: user )
       queue_item2 = Fabricate(:queue_item, video: video2, user: user )
+      [queue_item1, queue_item2]
     end
 
     context "With authenticated user" do
       before do
         session[:user_id] = user.id
+        get :index
       end
 
       it "sets @queue_items" do
-        expect(:queue_items).to match_array(queue_items)
+        expect(assigns(:queue_items)).to match_array(queue_items)
       end
     end
 
     context "With unauthenticated user" do
+      it "redirects to sign in page" do
+        get :index
+        expect(response).to redirect_to sign_in_path
+      end
     end
   end
 end
