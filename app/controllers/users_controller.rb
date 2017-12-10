7c3c268e44add6def09d050823263f1886f9fbe9
Taylor Peat <taylorpeat@hotmail.com>
2017-12-10 18:45:20 -0500

    Add mail feature and testing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
+      AppMailer.send_welcome_email(@user).deliver
       redirect_to videos_path
     else
       render :new
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -36,5 +36,34 @@ describe UsersController do
         expect(assigns(:user)).to be_instance_of(User)
       end
     end
+    
+    context "email sending" do
+      after { ActionMailer::Base.deliveries.clear }
+      
+      it "sends out the email" do
+        user_attributes = Fabricate.attributes_for(:user)
+        post :create, user: user_attributes
+        expect(ActionMailer::Base.deliveries).not_to be_empty
+      end
+
+      it "sends the email to the correct user" do
+        user_attributes = Fabricate.attributes_for(:user)
+        post :create, user: user_attributes
+        email = ActionMailer::Base.deliveries.last 
+        expect(email.to).to eq([user_attributes["email"]])
+      end
+
+      it "has the right content" do
+        user_attributes = Fabricate.attributes_for(:user)
+        post :create, user: user_attributes
+        email = ActionMailer::Base.deliveries.last 
+        expect(email.body).to include(user_attributes["full_name"])
+      end
+
+      it "does not send email with invalid inputs" do
+        post :create, user: {full_name: "Joe Schmoe", email: "j@s.com", password: "" }
+        expect(ActionMailer::Base.deliveries).to be_empty
+      end
+    end
   end
 end
