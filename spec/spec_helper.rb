7953df38a854d4bc462d7be43c740fb7c0a5116c
Taylor Peat <taylorpeat@hotmail.com>
2017-12-28 17:44:28 -0500

    Fix sidekiq testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,9 @@ require 'rspec/rails'
 require 'rspec/autorun'
 require 'capybara/email/rspec'
 require 'database_cleaner'
+require 'sidekiq/testing'
+
+Sidekiq::Testing.inline!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
