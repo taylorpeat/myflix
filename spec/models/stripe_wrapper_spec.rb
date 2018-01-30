e5956122159ae82b41c8b2947c8c422f96ca5949
Taylor Peat <taylorpeat@hotmail.com>
2018-01-30 17:48:59 -0500

    basic stripe wrapper and test for charge

--- spec/models/stripe_wrapper_spec.rb
+++ spec/models/stripe_wrapper_spec.rb
@@ -1,8 +1,29 @@
 require 'spec_helper'
 
-describe 'StripeWrapper' do
-  describe 'StripeWrapper::Charge' do
+describe StripeWrapper do
+  describe StripeWrapper::Charge do
     describe '.create', :vcr do
+      it "makes successful charge" do
+        Stripe.api_key = ENV["stripe_api_key"]
+
+        token = Stripe::Token.create(
+          :card => {
+            :number => "4242424242424242",
+            :exp_month => 1,
+            :exp_year => 2019,
+            :cvc => "314"
+          },
+        )
+
+        response = StripeWrapper::Charge.create(
+          amount: 999,
+          description: "Test",
+          token: token
+        )
+
+        expect(response.amount).to eq(999)
+        expect(response.description).to eq("Test")
+      end
     end
   end
 end
